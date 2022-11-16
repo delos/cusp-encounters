7f57927e02725ba89db49a5db8143b2ef9f0e3b8
Jens Stuecker <jens.stuecker@gmx.de>
2022-11-16 13:18:57 +0100

    Added milkyway orbits and some helper modules

---
+++ cusp_encounters/encounters_math.py
@@ -0,0 +1,197 @@
+import numpy as np
+import sys
+import os
+
+from scipy.spatial import cKDTree
+from scipy.special import factorial
+
+def weighted_percentile(x, perc = [10,50,90], weights=None):
+    isort = np.argsort(x)
+    
+    if weights is None:
+        weights = np.ones_like(x)
+    
+    x = x[isort]
+    weights = weights[isort]
+    
+    wtot = np.sum(weights)
+    
+    
+    wnew = np.insert(weights, [0,len(weights)], [0.,0.])
+    
+    wm = 0.5*(weights[1:] + weights[:-1])
+    
+    wmfrac = np.insert(np.cumsum(wm) / np.sum(wm), 0, 0.)
+    
+    return np.interp(np.array(perc)/100., wmfrac, x)
+
+def get_percentile_profile(x, y, perc=[0.15, 2.5, 16., 50, 84., 97.5, 99.85], xbins=None, weights=None, emptyval=np.nan):
+    if xbins is None:
+        xbins = np.logspace(-1,3, 81)
+        #xbins = np.linspace(np.min(x), np.max(x))
+    
+    xi = np.zeros(len(xbins) -1)
+    yi_perc = np.zeros((len(xbins) -1, len(perc)))
+    
+    for i in range(0, len(xbins)-1):
+        sel = (x >= xbins[i]) & (x < xbins[i+1])
+
+        n = np.sum(sel)
+
+        xi[i] = 0.5*(xbins[i]+xbins[i+1])
+
+        if n > 0:
+            if weights is None:
+                yi_perc[i] = np.percentile(y[sel], perc)
+            else:
+                yi_perc[i] = weighted_percentile(y[sel], perc, weights=weights[sel])
+        else:
+            yi_perc[i] = emptyval
+            
+    return xi, yi_perc
+
+def sample_closest_encounters_numerical(ndens, num=int(1e5), ntarget=int(1e5), nneigh=4):
+    L = np.sqrt(ntarget/ndens)
+
+    x = np.random.uniform(0., L, (ntarget, 2))
+    x2 = np.random.uniform(0., L, (int(1e5), 2))
+
+    mytree = cKDTree(x, boxsize=L)
+    r, xid = mytree.query(x2, k=nneigh)
+    return r
+
+def sample_closest_encounters_analytic(ndens, nsamp=None):
+    if nsamp is None:
+        nsamp = ndens.shape
+    
+    Fsamp = np.random.uniform(0., 1., nsamp)
+    
+    return np.sqrt(- np.log(1. - Fsamp) / ndens / np.pi)
+
+def knn_distribution(r, ndens, k=1):
+    """The pdf of the distance to the kth-nearest neighbor"""
+    lam = np.pi*r**2*ndens
+    return 2.*np.pi*ndens * r * np.exp(-lam) * lam**(k-1) / np.math.factorial(k-1)
+
+def cumulative_knn_distribution(r, ndens, k=1):
+    pows = np.arange(0, k)
+    
+    lam = np.array(ndens*np.pi*r**2)
+    
+    return 1. - np.exp(-lam) * np.sum(lam[...,np.newaxis]**pows / factorial(pows), axis=-1)
+
+def sample_strongest_B_analytic(chi, G, nsamp=None):
+    if nsamp is None:
+        nsamp = chi.shape
+    
+    Fsamp = np.random.uniform(0., 1., nsamp)
+    
+    return -(2.*np.pi*chi*G) / np.log(1. - Fsamp)
+
+def sample_encounters_B(realizations, Bmin, Bstar=1., sort=True):
+    """This is the new, good function!"""
+    Nexpect = Bstar / Bmin
+    ni = np.random.poisson(Nexpect, size=realizations)
+    nimax = np.max(ni)
+    
+    #print(nimax)
+    #print(ni)
+    assert nimax < 1e5
+    
+    Bmin, Bstar = np.array(Bmin), np.array(Bstar)
+    
+    x = np.random.uniform(0., (Bstar/Bmin)[...,np.newaxis], (realizations, nimax))
+    B = Bstar[...,np.newaxis] / x
+    
+    ntot = np.arange(0, nimax) * np.ones_like(B)
+    B[ntot > ni[:,np.newaxis]] = 0.
+    
+    if sort:
+        B = np.sort(B, axis=-1)[...,::-1]
+    
+    return B
+
+def sample_effective_B(realizations, Bstar=1., Bmin=None, p=1.2, sort=True):
+    if Bmin is None:
+        Bmin = Bstar*1e-3
+    return np.sum(sample_encounters_B(realizations, Bmin, Bstar=Bstar)**p, axis=-1)**(1./p)
+    
+
+import scipy.special
+def Jfit(B, a = 0.70836753, b=5.94572402, Bcore=1., alpha=1.24386139, beta=0.63723904):
+    x = B/Bcore
+    return 0.531 + np.log(a)/beta - (scipy.special.expi(-2.*alpha*a*(x + b*x**3)**(4./3.*beta) ) / beta  ) 
+def Jcorecusp_in_4piA2(B, Bcore, Bcusp, a = 0.70836753, b=5.94572402, alpha=1.24386139, beta=0.63723904):
+    x, xcusp = B/Bcore, B/Bcusp
+    
+    return np.clip(Jfit(B, Bcore=Bcore, a=a, b=b, alpha=alpha, beta=beta) + scipy.special.expi(-2.*alpha*xcusp**(beta*4./3.) ) / beta, 0., None)
+
+def Bresistance_of_r(r, A=1.):
+    G = 43.0071057317063e-4
+    return np.sqrt(8.*np.pi*G*A / (3*r**1.5))
+
+def rt_of_B(B, A):
+    G = 43.0071057317063e-4
+    return (8.*np.pi*G*A/3)**(2./3.) / B**(4./3.)
+
+import bacco.decorators as dc
+
+def sample_effective_B_hist(Nsamp, Bstar=1., Bminfac=1e-3, p=1.2, sort=True, initial_sample=100000, cachefile=None):
+    
+    @dc.h5cache(file=cachefile)
+    def get_hist_Beff(Bminfac, initial_sample, p):
+        Beff_for_hist = sample_effective_B(initial_sample, Bstar=1., Bmin=Bminfac, p=p)
+        return Beff_for_hist
+    Beff_for_hist = get_hist_Beff(Bminfac, initial_sample, p)
+    
+    rv_logBstar_hist = scipy.stats.rv_histogram(np.histogram(np.log10(Beff_for_hist), bins=np.linspace(-4, 7, 500)))
+    
+    return 10.**rv_logBstar_hist.rvs(size=Nsamp) * Bstar
+
+def angle_histogram(ri, mi, d=8, nalpha=100):
+    alpha = np.logspace(-2, np.log10(np.pi), nalpha)
+    mtot = np.zeros_like(alpha)
+    
+    # Loop to keep memory requirements reasonable
+    for i,alphai in enumerate(alpha):
+        r0 = (d*np.sin(alphai))
+        weights = np.zeros_like(ri)
+        if alphai < np.pi/2.:
+            def anglefac(r, r0):
+                return 1./(np.sqrt(r**2 - r0**2) * 4*np.pi*r)
+            
+            # The away side goes up to all radii
+            weights[ri > r0] = mi[ri > r0] * anglefac(ri[ri > r0], r0)
+            # The close-by side only contributes till d
+            weights[(ri > r0) & (ri < d)] += mi[(ri > r0) & (ri < d)] * anglefac(ri[(ri > r0) & (ri < d)], r0)
+        else:
+            weights[ri > d] = mi[ri > d] * anglefac(ri[ri > d], r0)
+        
+        mtot[i] = np.mean(weights)
+    
+    return alpha, mtot
+
+def percentile_plot(ri, perc, ax=None, ylabel=r"y", loc="best", cmap="viridis_r",vi =[0.,0.4,0.8]):
+    import matplotlib.pyplot as plt
+    if ax is None:
+        ax = plt.gca()
+    ax.fill_between(ri, perc[:,0], perc[:,-1], label=r"99.7% region", color=plt.get_cmap(cmap)(vi[0]))
+    ax.fill_between(ri, perc[:,1], perc[:,-2], label=r"95% region", color=plt.get_cmap(cmap)(vi[1]))
+    ax.fill_between(ri, perc[:,2], perc[:,-3], label=r"68% region", color=plt.get_cmap(cmap)(vi[2]))
+    ax.loglog(ri, perc[:,3], color="black", label="median")
+    plt.xlim(0.5,2e2)
+    ax.set_xlabel(r"$r$ [kpc]", fontsize=14)
+    ax.set_ylabel(ylabel, fontsize=14)
+    ax.grid("on")
+    ax.legend(loc=loc, fontsize=14)
+    
+def calculate_density_profile(r, m=None, rmin=1e-2, rmax=1e3, nbins=50):
+    if r.shape[-1] == 3: # position input
+        r = np.linalg.norm(r, axis=-1)
+    
+    rbins = np.logspace(np.log10(rmin),np.log10(rmax), nbins)
+    vbins = 4.*np.pi/3. * (rbins[1:]**3 - rbins[:-1]**3)
+    
+    ni, _ = np.histogram(r, bins=rbins, weights=m)
+    ri = np.sqrt(rbins[1:]*rbins[:-1])
+    return ri, ni/vbins
---
+++ cusp_encounters/milkyway.py
@@ -0,0 +1,411 @@
+import numpy as np
+import sys
+import os
+
+sys.path.append("../../adiabatic-tides")
+
+import adiabatic_tides as at
+
+def RvirOfMvir(mvir, delta=200., h=0.678, omega_m=0.308, omega_l=0.692, a=1.):
+    G = 43.0071057317063 * 1e-10  #  Grav. constant in Mpc (km/s)^2 / Msol
+    rhocrit = 3.0 / (8.0 * np.pi * G) * (1e2*h)**2 * (omega_m*a**-3 + omega_l)
+    
+    return  np.cbrt(mvir / (rhocrit * 4.*np.pi/3. * delta)) * 1e6  # in pc
+
+def potential_miyamoto_nagai(x, M=2e10, b=300e-6, a=3000e-6, G=43.0071057317063e-4):
+    """Miyamoto Nagai (1975) potential (https://articles.adsabs.harvard.edu/pdf/1975PASJ...27..533M)
+    see also http://astro.utoronto.ca/~bovy/AST1420/notes/notebooks/07.-Flattened-Mass-Distributions.html
+    """
+    # assuming the disk lies in the x-y plane
+    R = np.sqrt(np.square(x[...,0]) + np.square(x[...,1]))
+    z = x[...,2]
+    
+    return - G*M / np.sqrt(np.clip(R**2 + (np.sqrt(z**2 + b**2) + a)**2, 1e-20, None))
+
+def miyamoto_nagai_parameters_of_disk(M, Rd, b):
+    # See arxiv:1502.00627
+    coeffs = [{}, {}, {}]
+    coeffs[0]["M_over_Md"] = -0.0090, 0.0640, -0.1653, 0.1164, 1.9487
+    coeffs[1]["M_over_Md"] =  0.0173,-0.0903,  0.0877, 0.2029,-1.3077
+    coeffs[2]["M_over_Md"] = -0.0051, 0.0287, -0.0361,-0.0544, 0.2242
+    coeffs[0]["a_over_Rd"] = -0.0358, 0.2610, -0.6987,-0.1193, 2.0074
+    coeffs[1]["a_over_Rd"] = -0.0830, 0.4992, -0.7967,-1.2966, 4.4441
+    coeffs[2]["a_over_Rd"] = -0.0247, 0.1718, -0.4124,-0.5944, 0.7333
+    
+    pars = [{},{},{}]
+    for i in range(0,3):
+        x = b / np.clip(Rd, 1e-20, None)
+        xn = np.array(x)[..., np.newaxis]**np.array([4,3,2,1,0])
+        pars[i]["M"] = np.sum(xn*coeffs[i]["M_over_Md"], axis=-1) * M
+        pars[i]["a"] = np.sum(xn*coeffs[i]["a_over_Rd"], axis=-1) * Rd
+
+    return pars
+
+def disk_potential(x, M, Rd, b, G=43.0071057317063e-4):
+    # Approximation of Disk through three miyamato nagai potentials (arxiv:1502.00627)
+    pars = miyamoto_nagai_parameters_of_disk(M, Rd, b)
+    
+    pot = 0.
+    for i in range(0,3):
+        pot += potential_miyamoto_nagai(x=x, M=pars[i]["M"], b=b, a=pars[i]["a"], G=G)
+    return pot
+
+def finite_differences_n(x, f, deriv=1, h=1e-5, **kwargs):
+    """Calculate the nth order derivatives of a function
+    
+    x : locations where to evaluate the derivatives shape (..., ndim)
+    f : function f(x, **kwargs)
+    deriv : derivative, if > 1 this function will be called recursively
+    h : step-size that will be used for finite differences
+    **kwargs: will be passed through to the function
+    
+    returns : n-th derivative tensor, e.g. for deriv=2 shape = (..., ndim, ndim) 
+    """
+    if deriv==0:
+        return f(x, **kwargs)
+    
+    grad = np.zeros(np.shape(f(x, **kwargs)) + (3,)*deriv)
+    
+    dim = x.shape[-1]
+    eij = np.diag(np.ones(dim))
+    
+    for ax in range(0, dim):
+        xp = x + h * eij[ax]
+        xm = x - h * eij[ax]
+        
+        fp = finite_differences_n(xp, f, deriv=deriv-1, h=h, **kwargs)
+        fm = finite_differences_n(xm, f, deriv=deriv-1, h=h, **kwargs)
+        
+        grad[...,ax] = (fp - fm) / (2*h)
+
+    return grad
+
+class MilkyWay():
+    def __init__(self, h=0.679, adiabatic_contraction=True, cachedir=None):
+        self.h = h
+        self.G = G = 43.0071057317063 * 1e-4  #  Grav. constant in pc (km/s)^2 / Msol
+        self.cachedir = cachedir
+        
+        self.par = {}
+        self.par["halo"] = dict(mass = 1e12, conc=8.71)
+        self.par["stardisk"] = dict(mass = 4.100e+10, scalelength = 2.500e+03, thickness = 3.500e+02)
+        self.par["gasdisk"]  = dict(mass = 1.9e10, scalelength=7e3, thickness = 8e1)
+        self.par["bulge"]  = dict(mass = 9e9, scalelength=5e2)
+        
+        self.profile_nfw = at.profiles.NFWProfile(conc=self.par["halo"]["conc"], m200c=self.par["halo"]["mass"])
+        
+        self.adiabatic_contraction = adiabatic_contraction
+        if adiabatic_contraction:
+            # Create a spherically averaged mean baryon profile
+            # Using the MonteCarloProfile is a hack to use the
+            # already implemented potential calculation
+            rbins = np.logspace(-6, 1, 1000) * 200e3 
+            rcent = np.sqrt(rbins[1:]*rbins[:-1])
+            mcent = self.enclosed_mass(rbins[1:], components="sbg") - self.enclosed_mass(rbins[:-1], components="sbg")
+            self.profile_baryon_mean = at.profiles.MonteCarloProfile(rcent/1e6, mcent, rbins=rbins/1e6)
+            
+            if self.cachedir is None:
+                h5cache = None
+            else:
+                h5cache = "%s/contracted_nfw.hdf5" % self.cachedir
+            
+            self.profile_contracted_nfw = at.profiles.AdiabaticProfile(prof_initial=self.profile_nfw, 
+                                     prof_pert=self.profile_baryon_mean, 
+                                     niter=20,
+                                     verbose=True,
+                                     h5cache = h5cache)
+            self.profile_halo = self.profile_contracted_nfw
+        else:
+            self.profile_halo = self.profile_nfw
+        
+    def potential_halo(self, x):
+        #m200c, c = self.par["halo"]["mass"], self.par["halo"]["conc"]
+            
+        #r200c = RvirOfMvir(m200c, h=self.h, a=1.)
+        #rs = r200c / c
+        
+        #rhoc = m200c/(4.*np.pi*rs**3 * (np.log(1.+c) - c/(1.+c)))
+        #phi0 = - 4.*np.pi*self.G*rhoc*rs**2
+        
+        #u = np.linalg.norm(x, axis=-1) / rs
+        #phi0 * np.log(1. + u) / u, 
+        
+        #r = np.linalg.norm(x, axis=-1)
+
+        return self.profile_halo.self_potential(np.linalg.norm(x, axis=-1)/1e6)
+    
+    def potential_bulge(self, x):
+        M, a = self.par["bulge"]["mass"], self.par["bulge"]["scalelength"]
+
+        r = np.linalg.norm(x, axis=-1)
+        return - self.G * M / (r + a)
+    
+    def potential_disk(self, x,  mode="stardisk"):
+        assert mode in ("stardisk", "gasdisk")
+        
+        M, Rd, b = self.par[mode]["mass"], self.par[mode]["scalelength"], self.par[mode]["thickness"]
+
+        return disk_potential(x, M=M, Rd=Rd, b=b, G=self.G)
+    
+    def disk_surface_density(self, r, mode="stardisk"):
+        assert mode in ("stardisk", "gasdisk")
+        
+        M, Rd, b = self.par[mode]["mass"], self.par[mode]["scalelength"], self.par[mode]["thickness"]
+        
+        Sig = M / (2.*np.pi*Rd**2)
+        return Sig * np.exp(-r/Rd)
+    
+    def enclosed_mass_disk(self, r,  mode="stardisk"):
+        assert mode in ("stardisk", "gasdisk")
+        
+        M, Rd, b = self.par[mode]["mass"], self.par[mode]["scalelength"], self.par[mode]["thickness"]
+        
+        #r = np.linalg.norm(x, axis=-1)
+
+        return M * (1. - (r + Rd) / Rd * np.exp(- r / Rd) )
+    
+    def enclosed_mass_bulge(self, r):
+        M, a = self.par["bulge"]["mass"], self.par["bulge"]["scalelength"]
+        #r = np.linalg.norm(x, axis=-1)
+        
+        return M * r**2 / (r + a)**2
+    
+    def enclosed_mass_halo(self, r):
+        return self.profile_halo.self_m_of_r(r/1e6)
+    
+    def enclosed_mass(self, r, components="hbsg"):
+        mass = 0.
+        if "h" in components:
+            mass += self.enclosed_mass_halo(r)
+        if "b" in components:
+            mass += self.enclosed_mass_bulge(r)
+        if "s" in components:
+            mass += self.enclosed_mass_disk(r, mode="stardisk")
+        if "g" in components:
+            mass += self.enclosed_mass_disk(r, mode="gasdisk")
+        return mass
+
+    def potential(self, x, components="hbsg"):
+        potential = 0.
+        if "h" in components:
+            potential += self.potential_halo(x)
+        if "b" in components:
+            potential += self.potential_bulge(x)
+        if "s" in components:
+            potential += self.potential_disk(x, mode="stardisk")
+        if "g" in components:
+            potential += self.potential_disk(x, mode="gasdisk")
+        return potential
+    
+    def acceleration(self, x, components="hbsg", dx=0.1):
+        def pot(x):
+            return self.potential(x, components=components)
+        
+        return -finite_differences_n(x, pot, deriv=1, h=dx)
+    
+    def tidal_tensor(self, x, components="hbsg", dx=0.1):
+        def pot(x):
+            return self.potential(x, components=components)
+        
+        return -finite_differences_n(x, pot, deriv=2, h=dx)
+    
+    def density(self, x, components="hbsg", dx=0.1):
+        T = self.tidal_tensor(x, components=components, dx=dx)
+        
+        return - np.trace(T, axis1=-1, axis2=-2) / (4.*np.pi*self.G)
+    
+    def integrate_orbit(self, x0, v0, ti, components="hbsg"):
+        vfac = 978461.94   # year / (parsec / km/s)
+        
+        lastacc = 0.
+        
+        xi = np.zeros(ti.shape + x0.shape)
+        vi = np.zeros(ti.shape + v0.shape)
+        
+        xi[0], vi[0] = x0, v0
+
+        for i in range(1, len(ti)):
+            dt = (ti[i] - ti[i-1])
+            
+            # kick
+            if i == 1:
+                acc = self.acceleration(xi[i-1], components=components)
+            else:
+                acc = lastacc
+            vip = vi[i-1] + 0.5 * dt * acc / vfac
+            
+            # drift
+            xi[i] = xi[i-1] + vip * dt / vfac
+            
+            # kick
+            acc = self.acceleration(xi[i], components=components)
+
+            vi[i] = vip + 0.5 * dt * acc / vfac
+            
+            lastacc = acc
+        
+        return xi, vi
+    
+    def integrate_orbit_with_info(self, x0, v0, ti, components="hbsg", subsamp=10, with_info=False, verbose=False):
+        vfac = 978461.94   # year / (parsec / km/s)
+        
+        res = {}
+        for key in "pos", "vel", "acc":
+            res[key] = []
+        
+        x, v = np.copy(x0), np.copy(v0)
+        lastacc = np.zeros_like(x)
+        
+        if with_info:
+            last_dens_star, last_dens_dm, column_dens_star  = np.zeros(x0.shape[:-1]),np.zeros(x0.shape[:-1]),np.zeros(x0.shape[:-1])
+            chi_dm, chi_star, nperi  = np.zeros(x0.shape[:-1]),np.zeros(x0.shape[:-1]),np.zeros(x0.shape[:-1])
+            r2_m2, r2_m1  = np.sum(x**2,axis=-1), np.sum(x**2,axis=-1)
+            
+            for key in "dens_star", "dens_dm", "scolumndens", "chi_star", "chi_dm", "nperi":
+                res[key] = []
+        
+        def snap(i=0):
+            if verbose:
+                print("snap %d, step %d / %d" % (i/subsamp, i, len(ti)))
+            
+            res["pos"].append(np.copy(x))
+            res["vel"].append(np.copy(v))
+            res["acc"].append(np.copy(lastacc))
+            
+            if with_info:
+                res["dens_star"].append(np.copy(last_dens_star))
+                res["dens_dm"].append(np.copy(last_dens_dm))
+                res["scolumndens"].append(np.copy(column_dens_star))
+                res["chi_star"].append(np.copy(chi_star))
+                res["chi_dm"].append(np.copy(chi_dm))
+                res["nperi"].append(np.copy(nperi))
+            
+        snap()
+
+        for i in range(1, len(ti)):
+            dt = (ti[i] - ti[i-1])
+            
+            # kick
+            if i == 1:
+                acc = self.acceleration(x, components=components)
+            else:
+                acc = lastacc
+            v = v + 0.5 * dt * acc / vfac
+            
+            # drift
+            x = x + v * dt / vfac
+            
+            # kick
+            acc = self.acceleration(x, components=components)
+
+            v = v + 0.5 * dt * acc / vfac
+            
+            if with_info:
+                
+                dens_star = self.density(x, components="bs")
+                dens_dm = self.density(x, components="h")
+        
+                chi_star += 0.5*(dens_star + last_dens_star)*dt * 1.022012156719175e-06
+                chi_dm += 0.5*(dens_dm + last_dens_dm)*dt * 1.022012156719175e-06
+                
+                # The column density we just integrate less accurate, because we don't really need it
+                column_dens_star += dens_star * np.linalg.norm(v, axis=-1) * 1.0220e-06 * dt # pc/year / (km/s)   
+
+                r2 = np.sum(x**2, axis=-1)
+                nperi += (r2_m1 < r2) & (r2_m1 < r2_m2)
+                
+                last_dens_star, last_dens_dm = dens_star, dens_dm
+                r2_m2, r2_m1 = r2_m1, r2
+
+            lastacc = acc
+            
+            if i % subsamp == 0:
+                snap(i)
+                
+        for key in res:
+            res[key] = np.array(res[key])
+
+        return res
+    
+    def orbit_info(self, orbits):
+        res = {}
+
+        sdens = self.density(orbits["pos"], components="bs")
+
+        vel_pc_year = orbits["vel"] * 1.022012156719175e-06 # pc/year / (km/s)
+        df = sdens*np.linalg.norm(vel_pc_year, axis=-1)
+        
+        ti = orbits["t"]
+
+        scum = np.cumsum(0.5*(df[1:] + df[:-1])*(ti[1:,np.newaxis] - ti[:-1,np.newaxis]), axis=0)
+        res["sdens"] = sdens
+        res["scolumndens"] = np.insert(scum, 0, 0, axis=0)
+        del scum
+        
+        chi_cum = np.cumsum(0.5*(sdens[1:] + sdens[:-1])*(ti[1:,np.newaxis] - ti[:-1,np.newaxis]), axis=0)
+        res["chi"] = np.insert(chi_cum, 0, 0, axis=0) * 1.022012156719175e-06 # units of this are Msol / pc^3 * (pc/km) * s
+        del chi_cum
+
+        ri = np.linalg.norm(orbits["pos"], axis=-1)
+        nperi = np.sum((ri[1:-1] < ri[2:]) & (ri[1:-1] < ri[:-2]), axis=0)
+
+        res["nperi"] = np.sum((ri[1:-1] < ri[2:]) & (ri[1:-1] < ri[:-2]), axis=0)
+
+        return res
+    
+    def create_dm_orbits(self, ntot=10000, nsteps=10000, tmax=1e10, rmax=None, components="hbsg", seed=42, addinfo=False, subsamp=None, adaptive=False, verbose=False):
+        if rmax is None:
+            rmax = self.profile_nfw.r200c*1e6
+        
+        if self.cachedir is None:
+            h5cache = None
+        else:
+            import h5py
+            h5cache = "%s/milkyway_orbits.hdf5" % self.cachedir
+            h5path = "/orbit_rmax%.4e_ntot%d_seed%d_components%s_nsteps%d_tmax%.4e_xinfo%d" % (rmax/1e6, ntot, seed, components, nsteps, tmax, addinfo)
+            if subsamp is not None:
+                h5path = "/orbit_rmax%.4e_ntot%d_seed%d_components%s_nsteps%d_tmax%.4e_xinfo%d_sub%d" % (rmax/1e6, ntot, seed, components, nsteps, tmax, addinfo, subsamp)
+            if adaptive:
+                h5path += "_adaptive"
+        
+            if os.path.exists(h5cache):
+                with h5py.File(h5cache, "r") as h5file:
+                    #print(h5path)
+                    if h5path in h5file:
+                        print("reading")
+                        res = at.h5methods.h5py_read_dict(h5cache, path=h5path)
+
+                        return res
+        print("calculating")
+        
+        # calculation
+        np.random.seed(seed)
+        ti = np.linspace(0, tmax, nsteps)
+        
+        if adaptive:
+            def res_of_r(r):
+                return 1./r
+        else:
+            res_of_r = None
+        pos, vel, mass = self.profile_halo.sample_particles_uniform(ntot, rmax=rmax/1e6, nsteps_chain=1000, res_of_r=res_of_r)
+        
+        res = self.integrate_orbit_with_info(pos*1e6, vel, ti, components=components, with_info=addinfo, subsamp=subsamp, verbose=verbose)
+        res["mass"] = mass
+        #xi, vi = self.integrate_orbit(pos*1e6, vel, ti, components=components)
+        #res = dict(t=ti, pos=xi, vel=vi, mass=mass)
+        
+        #if addinfo:
+        #    res = {**res, **self.orbit_info(res)}
+            
+        #if subsamp is not None:
+        #    for key in res:
+        #        if (not key == "nperi") & (not key == "mass"):
+        #            res[key] = res[key][::subsamp]
+        
+        if h5cache is not None:
+            import h5py
+            with h5py.File(h5cache, "a") as h5file:
+                print("writing")
+                at.h5methods.h5py_write_dict(h5file, res, path=h5path, overwrite=True, verbose=1)
+                
+        return res
