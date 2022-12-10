fa99c0cdba5bff035109a5ac70380274ea240334
Jens Stuecker <jens.stuecker@gmx.de>
2022-12-10 16:57:47 +0100

    Added init.sh which helps with initializing some things

---
+++ init.sh
@@ -0,0 +1,4 @@
+# This is a short bash script to help setting up some things
+wget -O data/igrb_ackermann_2015.txt "https://cfn-live-content-bucket-iop-org.s3.amazonaws.com/journals/0004-637X/799/1/86/revision1/apj504089t3_mrt.txt?AWSAccessKeyId=AKIAYDKQL6LTV7YY2HIK&Expires=1670690788&Signature=%2Fzs%2BfilOfCNJhaaVxBu01EFnzHI%3D"
+mkdir -p caches
+mkdir -p notebooks/img
